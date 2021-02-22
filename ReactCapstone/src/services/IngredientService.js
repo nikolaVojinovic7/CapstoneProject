dcd33badadd0546162a5c236c496c7a4d50cbb7c
CDinatale <cora.dinatale@gmail.com>
2021-02-21 21:06:35 -0500

    fixed urls

--- ReactCapstone/src/services/IngredientService.js
+++ ReactCapstone/src/services/IngredientService.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
-const INGREDIENT_API_BASE_URL = "http://localhost:8087/api/ingredient";
+const INGREDIENT_API_BASE_URL = "http://10.0.2.2:8087/api/ingredient/";
 
 class IngredientService {
 
     getIngredients(){
-        return axios.get(USER_API_BASE_URL + '/' + "allIngredients");
+        return axios.get(INGREDIENT_API_BASE_URL + "allIngredients");
     }
 
     createIngredient(ingredient){
-        return axios.post(USER_API_BASE_URL + '/' + "createIngredient", ingredient);
+        return axios.post(INGREDIENT_API_BASE_URL + "createIngredient", ingredient);
     }
 
     getIngredientByName(name){
-        return axios.get(USER_API_BASE_URL + '/' + 'findIngredient' + '/' + name);
+        return axios.get(INGREDIENT_API_BASE_URL + 'findIngredient' + '/' + name);
     }
 
     updateIngredient(ingredient, name){
-        return axios.put(USER_API_BASE_URL + '/' + 'updateIngredient' + '/' + name, ingredient);
+        return axios.put(INGREDIENT_API_BASE_URL + 'updateIngredient' + '/' + name, ingredient);
     }
 
     deleteIngredient(name){
-        return axios.delete(USER_API_BASE_URL + '/' + 'deleteIngredient' + '/' + name);
+        return axios.delete(INGREDIENT_API_BASE_URL + 'deleteIngredient' + '/' + name);
     }
 }
 
--- ReactCapstone/src/services/RecipeService.js
+++ ReactCapstone/src/services/RecipeService.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
-const RECIPE_API_BASE_URL = "http://localhost:8087/api/recipe";
+const RECIPE_API_BASE_URL = "http://10.0.2.2:8087/api/recipe";
 
 class RecipeService {
 
     getRecipes(){
-        return axios.get(USER_API_BASE_URL + '/' + "allRecipes");
+        return axios.get(RECIPE_API_BASE_URL + '/' + "allRecipes");
     }
 
     createRecipe(user){
-        return axios.post(USER_API_BASE_URL + '/' + "createRecipe", recipe);
+        return axios.post(RECIPE_API_BASE_URL + '/' + "createRecipe", recipe);
     }
 
     getRecipeById(recipeId){
-        return axios.get(USER_API_BASE_URL + '/' + 'getRecipe' + '/' + recipeId);
+        return axios.get(RECIPE_API_BASE_URL + '/' + 'getRecipe' + '/' + recipeId);
     }
 
     updateRecipe(recipe, recipeId){
-        return axios.put(USER_API_BASE_URL + '/' + 'updateRecipe' + '/' + recipeId, recipe);
+        return axios.put(RECIPE_API_BASE_URL + '/' + 'updateRecipe' + '/' + recipeId, recipe);
     }
 
     deleteRecipe(recipeId){
-        return axios.delete(USER_API_BASE_URL + '/' + 'deleteRecipe' + '/' + recipeId);
+        return axios.delete(RECIPE_API_BASE_URL + '/' + 'deleteRecipe' + '/' + recipeId);
     }
 }
 
